296c3f0035100ecb713d0b1fbba551b90254fe04
ernilambar <nilambar@outlook.com>
2015-09-29 18:55:20 +0545

    remove admin scripts

--- archive.php
+++ archive.php
@@ -9,7 +9,7 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area col-md-8 col-sm-12 col-xs-12 <?php echo blue_planet_layout_setup_class(); ?>">
+	<section id="primary" class="content-area col-md-8 col-sm-12 col-xs-12 <?php echo esc_attr( blue_planet_layout_setup_class() ); ?>">
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
@@ -17,50 +17,53 @@ get_header(); ?>
 			<header class="page-header">
 				<h1 class="page-title">
 					<?php
-						if ( is_category() ) :
-							single_cat_title();
+					if ( is_category() ) :
+						single_cat_title();
 
 						elseif ( is_tag() ) :
 							single_tag_title();
 
 						elseif ( is_author() ) :
-							/* Queue the first post, that way we know
+							/*
+							Queue the first post, that way we know
 							 * what author we're dealing with (if that is the case).
 							*/
 							the_post();
-							printf( __( 'Author: %s', 'blue-planet' ), '<span class="vcard">' . get_the_author() . '</span>' );
-							/* Since we called the_post() above, we need to
+							printf( esc_html__( 'Author: %s', 'blue-planet' ), '<span class="vcard">' . get_the_author() . '</span>' );
+
+							/*
+							 * Since we called the_post() above, we need to
 							 * rewind the loop back to the beginning that way
 							 * we can run the loop properly, in full.
 							 */
 							rewind_posts();
 
 						elseif ( is_day() ) :
-							printf( __( 'Day: %s', 'blue-planet' ), '<span>' . get_the_date() . '</span>' );
+							printf( esc_html__( 'Day: %s', 'blue-planet' ), '<span>' . get_the_date() . '</span>' );
 
 						elseif ( is_month() ) :
-							printf( __( 'Month: %s', 'blue-planet' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
+							printf( esc_html__( 'Month: %s', 'blue-planet' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
 
 						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'blue-planet' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
+							printf( esc_html__( 'Year: %s', 'blue-planet' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
 
 						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-							_e( 'Asides', 'blue-planet' );
+							esc_html_e( 'Asides', 'blue-planet' );
 
 						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-							_e( 'Images', 'blue-planet');
+							esc_html_e( 'Images', 'blue-planet' );
 
 						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-							_e( 'Videos', 'blue-planet' );
+							esc_html_e( 'Videos', 'blue-planet' );
 
 						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-							_e( 'Quotes', 'blue-planet' );
+							esc_html_e( 'Quotes', 'blue-planet' );
 
 						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-							_e( 'Links', 'blue-planet' );
+							esc_html_e( 'Links', 'blue-planet' );
 
 						else :
-							_e( 'Archives', 'blue-planet' );
+							esc_html_e( 'Archives', 'blue-planet' );
 
 						endif;
 					?>
@@ -68,8 +71,8 @@ get_header(); ?>
 				<?php
 					// Show an optional term description.
 					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
+				if ( ! empty( $term_description ) ) :
+					printf( '<div class="taxonomy-description">%s</div>', $term_description ); // WPCS: XSS OK.
 					endif;
 				?>
 			</header><!-- .page-header -->
@@ -78,7 +81,9 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php
-					/* Include the Post-Format-specific template for the content.
+
+					/*
+				   * Include the Post-Format-specific template for the content.
 					 * If you want to override this in a child theme, then include a file
 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 					 */
