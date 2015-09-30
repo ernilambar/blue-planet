78909b60e55beaa6e4ae7cc8f079c08f2d18303c
ernilambar <nilambar@outlook.com>
2015-09-30 12:00:25 +0545

    fix functions file

--- functions.php
+++ functions.php
@@ -1,77 +1,83 @@
 <?php
-define( 'BS_THEME_NAME', 'Blue Planet' );
-define( 'BS_THEME_SLUG', 'blue-planet' );
-define( 'BS_THEME_VERSION', '2.0' );
-define( 'BS_SHORT_NAME', 'bsk' );
-?>
-<?php
 /**
  * Blue Planet functions and definitions
  *
  * @package Blue_Planet
  */
 
+define( 'BS_THEME_NAME', 'Blue Planet' );
+define( 'BS_THEME_SLUG', 'blue-planet' );
+define( 'BS_THEME_VERSION', '2.0' );
+define( 'BS_SHORT_NAME', 'bsk' );
+?>
+<?php
+
 if ( ! function_exists( 'blue_planet_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function blue_planet_setup() {
-
-    /**
-     * Set the content width based on the theme's design and stylesheet.
-     */
-    global $content_width;
-    if ( ! isset( $content_width ) ) {
-    	$content_width = 730; /* pixels */
-    }
-
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on Blue Planet, use a find and replace
-	 * to change 'blue-planet' to the name of your theme in all the template files
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
 	 */
-	load_theme_textdomain( 'blue-planet', get_template_directory() . '/languages' );
+	function blue_planet_setup() {
 
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+		/**
+	   * Set the content width based on the theme's design and stylesheet.
+	   */
+		global $content_width;
+		if ( ! isset( $content_width ) ) {
+			$content_width = 730;
+		}
 
-    // Add support for custom backgrounds
-    add_theme_support( 'custom-background' );
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on Blue Planet, use a find and replace
+		 * to change 'blue-planet' to the name of your theme in all the template files
+		 */
+		load_theme_textdomain( 'blue-planet', get_template_directory() . '/languages' );
 
-    // Add support for title tag
-    add_theme_support( 'title-tag' );
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
 
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 */
-	add_theme_support( 'post-thumbnails' );
-    add_image_size( 'homepage-thumb', 285, 215, true ); //(cropped)
+		// Add support for custom backgrounds.
+		add_theme_support( 'custom-background' );
 
-	// This theme uses wp_nav_menu()
-	register_nav_menus( array(
-        'primary' => __( 'Primary Menu', 'blue-planet' ),
-        'footer'  => __( 'Footer Menu', 'blue-planet' ),
-	) );
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
 
-	// Enable support for Post Formats.
-	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 */
+		add_theme_support( 'post-thumbnails' );
+		add_image_size( 'homepage-thumb', 285, 215, true ); // (cropped)
 
-	// Setup the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'blue_planet_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
+		// Register nav menu.
+		register_nav_menus( array(
+			'primary' => esc_html__( 'Primary Menu', 'blue-planet' ),
+			'footer'  => esc_html__( 'Footer Menu', 'blue-planet' ),
+		) );
 
-    // Load up theme options defaults
-	require( get_template_directory() . '/inc/blueplanet-themeoptions-defaults.php' );
+		// Enable support for Post Formats.
+		add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
 
-}
-endif; // blue_planet_setup
+		// Setup the WordPress core custom background feature.
+		add_theme_support( 'custom-background', apply_filters( 'blue_planet_custom_background_args', array(
+			'default-color' => 'ffffff',
+			'default-image' => '',
+		) ) );
+
+		// Load up theme options defaults.
+		require( get_template_directory() . '/inc/blueplanet-themeoptions-defaults.php' );
+
+	}
+endif;
 add_action( 'after_setup_theme', 'blue_planet_setup' );
 
 /**
@@ -79,82 +85,76 @@ add_action( 'after_setup_theme', 'blue_planet_setup' );
  */
 function blue_planet_widgets_init() {
 	register_sidebar( array(
-        'name'          => __( 'Sidebar', 'blue-planet' ),
-        'id'            => 'sidebar-1',
-        'description'   => __( 'Add widgets here to appear in your sidebar.', 'blue-planet' ),
-        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</aside>',
-        'before_title'  => '<h1 class="widget-title">',
-        'after_title'   => '</h1>',
+		'name'          => esc_html__( 'Sidebar', 'blue-planet' ),
+		'id'            => 'sidebar-1',
+		'description'   => esc_html__( 'Add widgets here to appear in your sidebar.', 'blue-planet' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
 	) );
-    register_sidebar( array(
-        'name'          => __( 'Menu Widget Area', 'blue-planet' ),
-        'id'            => 'sidebar-top-menu',
-        'description'   => __( 'Widget area in the header. Specially for menu widget', 'blue-planet' ),
-        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</aside>',
-        'before_title'  => '<h1 class="widget-title">',
-        'after_title'   => '</h1>',
-    ) );
+	register_sidebar( array(
+		'name'          => esc_html__( 'Menu Widget Area', 'blue-planet' ),
+		'id'            => 'sidebar-top-menu',
+		'description'   => esc_html__( 'Widget area in the header. Specially for menu widget', 'blue-planet' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
 
-    // Register footer widgets
-    $flg_enable_footer_widgets = blueplanet_get_option( 'flg_enable_footer_widgets' );
-    if( 1 == $flg_enable_footer_widgets){
-        $number_of_footer_widgets = blueplanet_get_option( 'number_of_footer_widgets' );
-        $num_footer = $number_of_footer_widgets;
+	// Register footer widgets.
+	$flg_enable_footer_widgets = blueplanet_get_option( 'flg_enable_footer_widgets' );
+	if ( 1 === $flg_enable_footer_widgets ) {
+		$number_of_footer_widgets = blueplanet_get_option( 'number_of_footer_widgets' );
+		$num_footer = $number_of_footer_widgets;
 
-        for($i = 1; $i <= $num_footer ;$i++){
-            register_sidebar(array(
-                'name'          => __('Footer Column','blue-planet') .'-'.$i,
-                'id'            => 'footer-area-'.$i,
-                'before_widget' => '<div id="%1$s" class="widget %2$s">',
-                'after_widget'  => '</div>',
-                'before_title'  => '<h1 class="footer-sidebar-title">',
-                'after_title'   => '</h1>',
-            ));
+		for ( $i = 1; $i <= $num_footer ;$i++ ) {
+			register_sidebar(array(
+				'name'          => esc_html__( 'Footer Column','blue-planet' ) .'-'.$i,
+				'id'            => 'footer-area-'.$i,
+				'before_widget' => '<div id="%1$s" class="widget %2$s">',
+				'after_widget'  => '</div>',
+				'before_title'  => '<h1 class="footer-sidebar-title">',
+				'after_title'   => '</h1>',
+			));
 
-        }
-    }
+		}
+	}
 
 }
 add_action( 'widgets_init', 'blue_planet_widgets_init' );
 
-/**
- * Enqueue scripts and styles.
- */
-if ( ! function_exists( 'blue_planet_scripts' ) ):
+if ( ! function_exists( 'blue_planet_scripts' ) ) :
 
-    function blue_planet_scripts() {
+	/**
+	 * Enqueue scripts and styles.
+	 */
+	function blue_planet_scripts() {
 
-        wp_enqueue_style( 'blue-planet-style', get_stylesheet_uri() );
-        wp_enqueue_style( 'blue-planet-style-bootstrap', get_template_directory_uri().'/css/bootstrap.min.css', false ,'3.0.0' );
-        wp_enqueue_style( 'blue-planet-style-responsive', get_template_directory_uri().'/css/responsive.css', false ,'' );
+		wp_enqueue_style( 'blue-planet-style', get_stylesheet_uri() );
+		wp_enqueue_style( 'blue-planet-style-bootstrap', get_template_directory_uri().'/css/bootstrap.min.css', false ,'3.0.0' );
+		wp_enqueue_style( 'blue-planet-style-responsive', get_template_directory_uri().'/css/responsive.css', false ,'' );
 
-    	wp_enqueue_script( 'blue-planet-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+		wp_enqueue_script( 'blue-planet-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
-    	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-    		wp_enqueue_script( 'comment-reply' );
-    	}
+		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+			wp_enqueue_script( 'comment-reply' );
+		}
 
-      $slider_status = blueplanet_get_option('slider_status');
-      $slider_status_2 = blueplanet_get_option('slider_status_2');
-      if( 'none' != $slider_status || 'none' != $slider_status_2 ){
-          //nivo
-          wp_enqueue_style( 'nivo-slider-style', get_template_directory_uri().'/thirdparty/nivoslider/nivo-slider.css', false ,'3.2' );
-          wp_enqueue_style( 'nivo-slider-style-theme', get_template_directory_uri().'/thirdparty/nivoslider/themes/default/default.css', false ,'3.2' );
-          wp_enqueue_script('nivo-slider-script',get_template_directory_uri().'/thirdparty/nivoslider/jquery.nivo.slider.pack.js', array('jquery'),'3.2', TRUE);
+		$slider_status = blueplanet_get_option( 'slider_status' );
+		$slider_status_2 = blueplanet_get_option( 'slider_status_2' );
+		if ( 'none' !== $slider_status || 'none' !== $slider_status_2 ) {
+			wp_enqueue_style( 'nivo-slider-style', get_template_directory_uri().'/thirdparty/nivoslider/nivo-slider.css', false ,'3.2' );
+			wp_enqueue_style( 'nivo-slider-style-theme', get_template_directory_uri().'/thirdparty/nivoslider/themes/default/default.css', false ,'3.2' );
+			wp_enqueue_script( 'nivo-slider-script',get_template_directory_uri().'/thirdparty/nivoslider/jquery.nivo.slider.pack.js', array( 'jquery' ),'3.2', true );
+		}
 
-      }
+		wp_enqueue_style( 'meanmenu-style', get_template_directory_uri().'/thirdparty/meanmenu/meanmenu.min.css', false ,'2.0.6' );
+		wp_enqueue_script( 'meanmenu-script',get_template_directory_uri().'/thirdparty/meanmenu/jquery.meanmenu.min.js', array( 'jquery' ),'2.0.6', true );
 
-      //meanmenu
-      wp_enqueue_style( 'meanmenu-style', get_template_directory_uri().'/thirdparty/meanmenu/meanmenu.min.css', false ,'2.0.6' );
-      wp_enqueue_script('meanmenu-script',get_template_directory_uri().'/thirdparty/meanmenu/jquery.meanmenu.min.js', array('jquery'),'2.0.6', TRUE);
-
-
-
-      //theme custom
-      wp_enqueue_script('blue-planet-theme-script-custom',get_template_directory_uri().'/js/custom.js', array('jquery'),'1.1.1', TRUE);
-    }
+		wp_enqueue_script( 'blue-planet-theme-script-custom',get_template_directory_uri().'/js/custom.js', array( 'jquery' ),'1.1.1', true );
+	}
 
 endif;
 
