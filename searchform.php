be376da224a3cd38a05ba526621e308459fcfd00
ernilambar <nilambar@outlook.com>
2015-10-01 17:24:19 +0545

    fix text domain issue

--- searchform.php
+++ searchform.php
@@ -11,7 +11,7 @@
 ?>
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<fieldset>
-		<span class="screen-reader-text"><?php echo esc_html__( 'Search for:', 'label', 'blue-planet' ); ?></span>
+		<span class="screen-reader-text"><?php echo esc_html_x( 'Search for:', 'label', 'blue-planet' ); ?></span>
 		<input type="search" class="search-field" placeholder="<?php echo esc_attr( $search_placeholder ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
   <input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'blue-planet' ); ?>">
 	</fieldset>
