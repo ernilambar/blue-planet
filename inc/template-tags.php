68c2d22871d0983bb20244973c612bde50bfd95f
ernilambar <nilambar@outlook.com>
2016-04-12 13:06:23 +0545

    update mean menu

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -35,7 +35,7 @@ function blue_planet_posted_on() {
 
 	if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 		echo '<span class="comments-link comments">';
-		comments_popup_link( esc_html__( '0 comment', '_s' ), esc_html__( '1 Comment', '_s' ), esc_html__( '% Comments', '_s' ) );
+		comments_popup_link( esc_html__( '0 comment', 'blue-planet' ), esc_html__( '1 Comment', 'blue-planet' ), esc_html__( '% Comments', 'blue-planet' ) );
 		echo '</span>';
 	}
 
