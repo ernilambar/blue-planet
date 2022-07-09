ebeaf24a935b1650da22239547e75fe504639a7f
Nilambar Sharma <nilambar@outlook.com>
2022-07-09 18:44:58 +0545

    Fix skip link

--- inc/theme-custom.php
+++ inc/theme-custom.php
@@ -505,3 +505,21 @@ function blue_planet_add_admin_notice() {
 }
 
 add_action( 'admin_init', 'blue_planet_add_admin_notice' );
+
+/**
+ * Fix skip link focus in IE11.
+ *
+ * This does not enqueue the script because it is tiny and because it is only for IE11,
+ * thus it does not warrant having an entire dedicated blocking script being loaded.
+ *
+ * @link https://git.io/vWdr2
+ */
+function blue_planet_skip_link_focus_fix() {
+	// The following is minified via `terser --compress --mangle -- js/skip-link-focus-fix.js`.
+	?>
+	<script>
+	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
+	</script>
+	<?php
+}
+add_action( 'wp_print_footer_scripts', 'blue_planet_skip_link_focus_fix' );
