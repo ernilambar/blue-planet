9730d24c354dfb1f7952770ce53d9405ecda8030
Nilambar Sharma <nilambar@outlook.com>
2024-08-10 14:29:26 +0545

    Update packages

--- js/customize-controls.js
+++ js/customize-controls.js
@@ -1,6 +1,4 @@
 ( function ( $, api ) {
-	/* === Dropdown Taxonomies Control === */
-
 	api.controlConstructor[ 'dropdown-taxonomies' ] = api.Control.extend( {
 		ready: function () {
 			var control = this;
